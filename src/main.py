e55ed5ae3a04c9b77c0a25e25df03c834e79c124
jimcturner <james@jamescturner.co.uk>
2023-02-17 14:23:50 +0000

    adds tests folder

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
 def main():
     print("Thanks for running me")
-# Invoke main() method (entry point for Python script)
 
+
+def intDoubler(x: int)->str:
+    """
+        Takes an int x.
+        Returns 2 *x in as a string padded with zeroes
+    :param x:
+    :return:
+    """
+    return str(x * 2).zfill(3)
+
+# Invoke main() method (entry point for Python script)
 if __name__ == "__main__":
     # Call main
     main()
---
+++ tests/main_tests.py
@@ -0,0 +1,28 @@
+import sys
+import unittest
+
+sys.path.append("..") # Fix to allow unittest to search for modules/paths in the parent folder
+import src.main
+### How to run these tests:
+# From within *this* folder (i.e tests/) use
+#
+#       python -m main_tests    *OR*    python main_tests.py
+#
+# If running from the (parent) project folder use
+#
+#       python -m tests.main_tests *OR* python -m unittest discover tests "*_tests.py"
+
+class Main_Tests(unittest.TestCase):
+    def setUp(self) -> None:
+        print("Tests set up")
+
+    def test_intDoubler(self):
+        self.assertEqual("004", src.main.intDoubler(2))
+
+    def tearDown(self) -> None:
+        print("Tests teardown")
+
+
+# We need the following so it will execute the tests when we run the file in python
+if __name__ == '__main__':
+    unittest.main()
